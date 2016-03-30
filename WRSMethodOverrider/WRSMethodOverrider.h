d01fb7cf7763e63634a3df0b258e9b48e902c8d9
Wrsford <willstafford@me.com>
2016-03-30 09:12:20 -0400

    Initial Commit

---
+++ WRSMethodOverrider/WRSMethodOverrider.h
@@ -0,0 +1,19 @@
+//
+//  WRSMethodOverrider.h
+//  WRSMethodOverrider
+//
+//  Created by Will Stafford on 3/30/16.
+//  Copyright © 2016 wrsford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WRSMethodOverrider.
+FOUNDATION_EXPORT double WRSMethodOverriderVersionNumber;
+
+//! Project version string for WRSMethodOverrider.
+FOUNDATION_EXPORT const unsigned char WRSMethodOverriderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WRSMethodOverrider/PublicHeader.h>
+
+
